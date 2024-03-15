62363044de482844f0c768fe92644af61981aa24
Ulijaym <162015275+Aplosy@users.noreply.github.com>
2024-03-15 09:09:27 -0600

    Add files via upload
    practica de generacion de rfc

---
+++ prueba/src/main/java/logica/Prueba1.java
@@ -0,0 +1,15 @@
+package logica;
+
+import igu.Pantalla;
+
+public class Prueba1 {
+
+    public static void main(String[] args) {
+        
+        Pantalla panta = new Pantalla();
+        panta.setVisible(true); 
+        panta.setLocationRelativeTo(null);
+        
+    }
+    
+}
